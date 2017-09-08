caf56053079cf228e8f9a10854c615b9bc356470
Denis Suvolokin <budden@list.ru>
2017-09-08 17:39:48 +0400

    trying to move by buttons NOT WORK
    разобраться, как снимать данные с кнопок и с форм

--- lift_app.rb
+++ lift_app.rb
@@ -3,8 +3,9 @@ require 'sinatra'
 require 'shotgun'
 require_relative 'lift_class.rb'
 
+l=Lift.new 
+
 get '/' do
-	l=Lift.new 
 	@current=l.current_floor
 	@floor=@current.to_s
 	erb :index
@@ -12,4 +13,11 @@ get '/' do
 	# l.destination_floor=gets.chomp.to_i 
 	# l.move if l.check_floor 
 	# l.show_floor
+end
+
+post '/' do
+	@destination_floor=params[:destination_floor].to_i
+	l.destination_floor=@destination_floor
+	l.move
+	erb :index
 end
